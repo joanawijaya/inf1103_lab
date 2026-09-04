18e3f3844264c257d5faad3372f2bd181a481037
Juyoie <joanahuang09@gmail.com>
2026-09-04 15:16:40 +0800

    Fourth Post

--- hello.py
+++ hello.py
@@ -7,15 +7,19 @@ username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
 
-followers += 50
-print("Day 1: ", followers)
+#followers += 50
+#print("Day 1: ", followers)
 
-followers += 20
-print("Day 2: ", followers)
+#followers += 20
+#print("Day 2: ", followers)
 
-followers += 10
-print("Day 3: ", followers)
+#followers += 10
+#print("Day 3: ", followers)
 
 #print ("userame: ", username)
 #print ("Bio: ", bio)
 #print ("Followers: ", followers)
+
+username = input("enter username: ")
+age = input("Enter age: ")
+category = input("enter content category: ")
