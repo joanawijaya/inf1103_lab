c05d0a8f3ed96c77d97220f0731015d99e60a90c
Juyoie <joanahuang09@gmail.com>
2026-09-04 15:05:42 +0800

    second post

---
+++ hello.py
@@ -0,0 +1,12 @@
+print ("====================")
+print ("welcome here")
+print ("my first post!")
+print ("====================")
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print ("userame: ", username)
+print ("Bio: ", bio)
+print ("Followers: ", followers)
