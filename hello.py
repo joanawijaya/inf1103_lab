351514e36c0d06a67e77eeb2d24b8b71167ea41c
Juyoie <joanahuang09@gmail.com>
2026-09-04 15:10:48 +0800

    third post

--- hello.py
+++ hello.py
@@ -7,6 +7,15 @@ username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
 
-print ("userame: ", username)
-print ("Bio: ", bio)
-print ("Followers: ", followers)
+followers += 50
+print("Day 1: ", followers)
+
+followers += 20
+print("Day 2: ", followers)
+
+followers += 10
+print("Day 3: ", followers)
+
+#print ("userame: ", username)
+#print ("Bio: ", bio)
+#print ("Followers: ", followers)
